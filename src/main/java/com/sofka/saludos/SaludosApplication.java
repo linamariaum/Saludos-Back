9bcd6fec83c00cad200456341409c54106ba5a5b
Lina Uribe <lina8452@gmail.com>
2021-06-17 13:43:01 -0500

    Creacion del proyecto

---
+++ src/main/java/com/sofka/saludos/SaludosApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.saludos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaludosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofka/saludos/SaludosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.saludos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
